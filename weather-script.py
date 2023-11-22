5255e3d92abd77da0fad2a82316752f9ff4d2c2a
sapirev <sapir.eliyahu12@gmail.com>
2023-11-22 22:14:56 +0200

    new: pushing all the files

--- weather-script.py
+++ weather-script.py
@@ -1,51 +1,24 @@
+from time import sleep
+
 import requests
 from elasticsearch import Elasticsearch
 from datetime import datetime, timezone
 import os
 from kubernetes import client, config
 import base64
+from requests.auth import HTTPBasicAuth
 
 
-def fetch_weather(api_key, city, metadata):
+def fetch_weather(api_key, city):
     try:
         url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
         response = requests.get(url)
         response.raise_for_status()  # Raise an HTTPError for bad responses
         data = response.json()
-        return data, metadata
+        return data
     except requests.exceptions.RequestException as e:
         print(f"Error fetching weather data for {city}: {e}")
-        return None, metadata
-
-
-def get_node_name():
-    try:
-        response = requests.get("https://kubernetes.default.svc.cluster.local:50900/api/v1/nodes", headers={
-            "Authorization": f"Bearer {open('/run/secrets/kubernetes.io/serviceaccount/token').read().strip()}"})
-
-        response.raise_for_status()
-        node_name = response.json()["items"][0]["metadata"]["name"]
-        return node_name
-    except Exception as e:
-        print(f"Error retrieving node name: {e}")
         return None
-
-
-def get_pod_and_node_name():
-    try:
-        with open("/var/run/secrets/kubernetes.io/serviceaccount/namespace", "r") as file:
-            namespace = file.read().strip()
-
-        with open("/var/run/secrets/kubernetes.io/serviceaccount/podname", "r") as file:
-            pod_name = file.read().strip()
-
-        with open("/var/run/secrets/kubernetes.io/serviceaccount/nodename", "r") as file:
-            node_name = file.read().strip()
-
-        return {"pod_name": pod_name, "namespace": namespace, "node_name": node_name}
-    except Exception as e:
-        print(f"Error retrieving pod and node name: {e}")
-        return {}
 
 
 def push_to_elasticsearch(es, index, document):
@@ -66,13 +39,10 @@ def get_api_key_from_secret():
         secret = v1.read_namespaced_secret(secret_name, "default")
         api_key_base64 = secret.data.get("api_key", None)
 
-        print(f"Base64-encoded API key: {api_key_base64}")
-
         if api_key_base64:
             api_key_bytes = base64.b64decode(api_key_base64)
             api_key = api_key_bytes.decode("utf-8").strip()
 
-            print(f"API key: {api_key}")
             return api_key
 
     except Exception as e:
@@ -81,26 +51,63 @@ def get_api_key_from_secret():
     return None
 
 
+def get_elasticsearch_credentials():
+    try:
+        config.load_incluster_config()  # Load in-cluster config for running inside a pod
+        v1 = client.CoreV1Api()
+        secret_name = "elasticsearch-master-credentials"
+
+        # Retrieve the secret data
+        secret = v1.read_namespaced_secret(secret_name, "default")
+        es_username_base64 = secret.data.get("username", None)
+        es_password_base64 = secret.data.get("password", None)
+
+        if es_username_base64 and es_password_base64:
+            es_username = base64.b64decode(es_username_base64).decode("utf-8").strip()
+            es_password = base64.b64decode(es_password_base64).decode("utf-8").strip()
+
+            return es_username, es_password
+
+    except Exception as e:
+        print(f"Error retrieving Elasticsearch credentials from Kubernetes secret: {e}")
+
+    return None, None
+
+
 def main():
     api_key = get_api_key_from_secret()
     if not api_key:
         print("API key not found in Kubernetes secret.")
         return
 
-    es = Elasticsearch([{"host": "elasticsearch-master.default.svc.cluster.local", "port": 9200, "scheme": "http"}])
-    index = "weather-data"
+    es_username, es_password = get_elasticsearch_credentials()
 
+    # Check if credentials are available
+    if es_username and es_password:
+        es = Elasticsearch(
+            [{"host": "elasticsearch-master.default.svc.cluster.local", "port": 9200, "scheme": "http"}],
+            basic_auth=(es_username, es_password),
+        )
+        index = "weather-data"
+    else:
+        es = ""
+        index = "weather-data"
     cities = ["Jerusalem", "New York", "London", "Paris", "Tokyo"]
-    metadata = {"additional_info": "your_metadata_here"}
 
-    node_name = get_node_name()
-    print(f"Node Name: {node_name}")
+    node_name = os.environ.get('NODE_NAME')
+    pod_name = os.environ.get('POD_NAME')
 
-    pod_and_node_metadata = get_pod_and_node_name()
-    metadata.update(pod_and_node_metadata)
+    metadata = {"pod_name": pod_name, "node_name": node_name}
+
+    # node_name = get_node_name()
+    # print(f"Node Name: {node_name}")
+
+    # pod_and_node_metadata = get_pod_and_node_name()
+    # metadata.update(pod_and_node_metadata)
 
     for city in cities:
-        weather_data, metadata = fetch_weather(api_key, city, metadata)
+        # weather_data, metadata = fetch_weather(api_key, city, metadata)
+        weather_data = fetch_weather(api_key, city)
         if weather_data:
             timestamp = datetime.now(timezone.utc).isoformat()
             document = {
@@ -111,7 +118,9 @@ def main():
                 "weather_description": weather_data["current"]["condition"]["text"],
                 "metadata": metadata,
             }
+            print(f"document: {document}")
             push_to_elasticsearch(es, index, document)
+        sleep(30)
 
 
 if __name__ == "__main__":
